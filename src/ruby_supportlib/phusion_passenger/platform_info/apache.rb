3a4459e746d81a5784a17258f41401b1b0cc31e6
Camden Narzt <c.narzt@me.com>
2025-07-12 11:01:39 -0600

    Deal with frozen string literals
    In future Ruby is going to encourage frozen string literals

--- src/ruby_supportlib/phusion_passenger/platform_info/apache.rb
+++ src/ruby_supportlib/phusion_passenger/platform_info/apache.rb
@@ -729,7 +729,7 @@ module PhusionPassenger
     # Linker flags that are necessary for linking an Apache module.
     # Already includes APR and APU linker flags.
     def self.apache2_module_cxx_ldflags
-      flags = ""
+      flags = String.new
       if !apxs2.nil?
         flags << `#{apxs2} -q LDFLAGS`.strip
       end
